fe99875dbb352a535f48c81346297cdf038cf890
Gianmarco-Cellamare <gianmarco.cellamare@marconibari.edu.it>
2026-04-27 14:10:59 +0000

    Esercizio

--- Esercizio.java
+++ Esercizio.java
@@ -3,11 +3,101 @@ import java.util.Scanner;
 
 // Classe principale, con metodo main
 class Esercizio {
-    Scanner in=new Scanner (System.in)
-    do {
-    System.out.print("Inserisci parola: ");
-    String parola = in.nextLine();
-    do{
+    class Nodo{
+        T dato;
+        Nodo <T> next;
+    }
+
+    public Nodo <T>(T dato){
+        this.dato=dato;
+        this.next=null;
+    }
+
+    class Pila <T> {
+        private Nodo<T> top;
+        public Pila (){
+            this.top=null;
+        } 
+    }
+
+    
+
+
+
+    public void push (T dato){
+        Nodo<T> nuovoNodo=new Nodo (dato);
+        nuovoNodo=top;
+        top=nuovoNodo;
+    }
+
+    public T pop (){
+        if(top==null){
+            throw.newNoSuchElementException("La pila è vuota");
+        }
+        T dato=top.dato;
+        top=top.next;
+        return dato;
+    }
+
+    public T peak(T dato){
+        if (top==null){
+            throw.newNoSuchElementException("La pila è vuota");
+        }
+        T dato=top.dato;
+        return dato;
+    }
+
+    public boolean isEmpty(){
+        return (top==null);
+    }
+
+    public int size(){
+        curr=0;
+        Nodo<T>current=top;
+        while (current!=null){
+            current=current.next;
+            ++curr;
+        }
+        return curr;
+    }
+
+    Public String toString(){
+        String str="[";
+        Nodo<T> current=top;
+        while (current.next !=null){
+            current=current.next;
+            str+=curr.dato;
+        }
+
+        str="]";
+
+    }
+
+    public static void (main args[]){
+        Scanner in=new Scanner();
+        String parola;
+        System.out.println("Inserisci parola");
+        parola=in.nextLine(System.in);
+        for(int i=0;i<parola.length();i++){
+            parola.charAt(i); //restituisce il singolo carattere in una posizione specifica
+            pila.push();  
+        }
+        String parolaCheck="";
+        if (!pila.isEmpty()){
+            parolaCheck+=pila.pop();
+        }
+
+        if(parola.equals(parolaCheck)){
+            System.out.println("La parola è palindroma");
+        }else{
+            System.out.println("La parola non è palindroma");
+        }
+
+
+    }
+
+    }
+
 
         
 
